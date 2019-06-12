5fa55acd59461fc4307b15e0fdab10604c8e54f2
andrewhwaller <andrewhwaller@gmail.com>
2019-06-12 12:39:38 -0500

    Create basic sessions controller/login/logout routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  def hello
+  end
+
+  def current_user
+  end
+
+  private
+
+  def require_logged_in
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
   get 'site/index'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
 
   get 'home', to: 'site#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
