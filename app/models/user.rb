21fe7fe166e80bca839c0137e1120331a8f1bc37
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2019-06-12 16:49:33 -0500

    Add extra devise tags to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :omniauthable, :omniauth_providers => [:google_oauth2]
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :omniauthable, :omniauth_providers => [:google_oauth2]
 
   def self.from_omniauth(auth)
   # Either create a User record or update it based on the provider (Google) and the UID   
