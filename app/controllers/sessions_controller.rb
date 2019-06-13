933f3fa3981ef2d806f42195a2ee5af5f02c9f96
andrewhwaller <andrewhwaller@gmail.com>
2019-06-13 14:39:20 -0500

    Add devise user views

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,7 @@
 class SessionsController < ApplicationController
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
   
   def new
-    render layout: 'login'
   end
 
   def create
