f65c68f555334fe8c43c9dfb0e75d4518a84292e
andrewhwaller <andrewhwaller@gmail.com>
2019-06-13 15:22:26 -0500

    Add working devise account creation/login

--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,10 @@ ActiveRecord::Schema.define(version: 2019_06_12_214134) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "provider"
@@ -29,6 +33,7 @@ ActiveRecord::Schema.define(version: 2019_06_12_214134) do
     t.integer "expires_at"
     t.boolean "expires"
     t.string "refresh_token"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
