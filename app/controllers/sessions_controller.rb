c1ad5effcdb43fbee676620c6cdbd0fcbf9af0ae
andrewhwaller <andrewhwaller@gmail.com>
2019-06-13 14:28:54 -0500

    Populate view in index.html.erb; add yield to application.html.erb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,8 @@
 class SessionsController < ApplicationController
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
   
   def new
-    render :layout => 'login'
+    render layout: 'login'
   end
 
   def create
