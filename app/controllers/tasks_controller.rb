35ae322f6e7720e4d8ec2edf896f46f45c151b81
andrewhwaller <andrewhwaller@gmail.com>
2019-06-14 16:26:38 -0500

    Build out CR for tasks; build functioning new task form; add complete boolean and user_id to tasks table

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,25 @@
 class TasksController < ApplicationController
 
   def index
+    @task = Task.new
+    @tasks = current_user.tasks.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.user_id = current_user.id
+    @task.save
+
+    redirect_to '/'
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name)
   end
 end
