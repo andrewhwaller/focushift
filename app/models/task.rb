424686f978c420548bcab5227e2eb652d1b51e4f
andrewhwaller <andrewhwaller@gmail.com>
2019-06-15 15:13:32 -0500

    Add devise views/styled login view

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :user
+  belongs_to :project
   validates :name, :presence => true
 
   STATUS = {
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_15_045042) do
+ActiveRecord::Schema.define(version: 2019_06_15_185922) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "projects", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "name"
