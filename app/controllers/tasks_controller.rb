be32c8ddc9a82750b9f0946d091ae62f24c90efe
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2019-06-18 11:20:19 -0500

    Remove raise params/comment out task.rb belongs_to project for testing purposes

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -15,8 +15,6 @@ class TasksController < ApplicationController
     @task.user_id = current_user.id
     if @task.save
       redirect_to '/'
-    else 
-      raise params.inspect
     end
   end
 
