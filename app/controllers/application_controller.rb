02b92a457b1d8514f9d8317991ae130928d0ce83
andrewhwaller <andrewhwaller@gmail.com>
2019-06-19 17:39:48 -0500

    Add devise permitted first_name/last_name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery prepend: true
   before_action :authenticate_user!
+  before_action :set_active_user
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password)}
+
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :email, :password, :current_password)}
+  end
 
   private
 
@@ -9,5 +19,9 @@ class ApplicationController < ActionController::Base
 
   def set_active_user
     @active_user = current_user
+  end
+
+  def user_full_name
+    @user_full_name = (@active_user.first_name + " " + @active_user.last_name)
   end
 end
