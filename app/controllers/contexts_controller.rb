d0ac112f2eebf306ee9b3d627be03f2aa3505049
andrewhwaller <andrewhwaller@gmail.com>
2019-07-02 14:20:17 -0500

    Add contexts controller

---
+++ app/controllers/contexts_controller.rb
@@ -0,0 +1,49 @@
+class ContextsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_active_user
+  
+  def index
+    @context = Context.new
+    @contexts = current_user.contexts.all.where("status = '0'")
+  end
+
+  def show
+    @context = Context.find(params[:id])
+  end
+
+  def new
+    @context = Context.new
+  end
+
+  def create
+    @context = Context.new(context_params)
+    @context.user_id = current_user.id
+    if @context.save
+      redirect_to contexts_path
+    else
+      raise params.inspect
+    end
+  end
+
+  def edit
+    @context = Context.find(params[:id])
+  end
+
+  def update
+    @context = Context.find(params[:id])
+    @context.update(context_params)
+    redirect_to contexts_path
+  end
+
+  def destroy
+    @context = Context.find(params[:id])
+    @context.destroy
+    redirect_to contexts_path
+  end
+
+  private
+
+  def context_params
+    params.require(:context).permit!
+  end
+end
