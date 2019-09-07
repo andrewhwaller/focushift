f226b1e68d6af8b3f669e3f24c65967db28a0378
andrewhwaller <andrewhwaller@gmail.com>
2019-09-06 22:39:03 -0500

    Add working sign out link to dropdown

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   resources :tasks
   resources :contexts
   devise_for :users, :controllers => { omniauth_callbacks: "users/omniauth_callbacks" }
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 
   root to: 'tasks#index'
 
