a17f8e08406c7273380e8b5bfc87ae5a22b9c7da
andrewhwaller <andrewhwaller@gmail.com>
2019-09-06 23:46:11 -0500

    Add first/last name to from_omniauth method in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,8 @@ class User < ApplicationRecord
       user.expires_at = auth.credentials.expires_at
       user.refresh_token = auth.credentials.refresh_token
       user.email = auth.info.email
+      user.first_name = auth.info.first_name
+      user.last_name = auth.info.last_name
       user.provider = auth.provider
       user.uid = auth.uid
       user.password = Devise.friendly_token[0,20]
