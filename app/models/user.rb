b32a5e6cbb4884a74eb6787810ae529ee71c309a
andrewhwaller <andrewhwaller@gmail.com>
2019-09-20 15:34:50 -0500

    Update from_omniauth method to include first/last name

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,13 +10,14 @@ class User < ApplicationRecord
   has_many :partners, through: :partnerships
 
   def self.from_omniauth(auth)
-  # Either create a User record or update it based on the provider (Google) and the UID   
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.token = auth.credentials.token
       user.expires = auth.credentials.expires
       user.expires_at = auth.credentials.expires_at
       user.refresh_token = auth.credentials.refresh_token
       user.email = auth.info.email
+      user.first_name = auth.info.first_name
+      user.last_name = auth.info.last_name
       user.provider = auth.provider
       user.uid = auth.uid
       user.password = Devise.friendly_token[0,20]
