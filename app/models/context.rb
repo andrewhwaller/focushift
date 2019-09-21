90f380394d03d3d33cc62c8c8f7bf436077b4d4c
andrewhwaller <andrewhwaller@gmail.com>
2019-09-20 22:07:32 -0500

    Add validations to partnership and context

--- app/models/context.rb
+++ app/models/context.rb
@@ -1,4 +1,6 @@
 class Context < ApplicationRecord
   belongs_to :user
   has_and_belongs_to_many :projects
+
+  validates :name, :presence => true
 end
--- app/models/partnership.rb
+++ app/models/partnership.rb
@@ -2,4 +2,7 @@ class Partnership < ApplicationRecord
   belongs_to :user
   belongs_to :partner, class_name: "User"
   has_and_belongs_to_many :projects
+
+  validates :name, :presence => true
+  validates :partner_id, :presence => true, :type => Integer
 end
