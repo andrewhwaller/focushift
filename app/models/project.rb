0c7601305e0360ad860feccae5ac82e5bfa5bec5
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2019-09-21 10:40:20 -0500

    Add errors iterator to edit task

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,7 +4,7 @@ class Project < ApplicationRecord
   has_and_belongs_to_many :contexts
   has_and_belongs_to_many :partnerships
 
-  validates :name, presence: { message: "cannot be blank" }
+  validates :name, presence: { message: "cannot be blank." }
   validates :partnership_id, numericality: { message: "Something seems wrong with %{attribute}!" }
 
   STATUS = {
