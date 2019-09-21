2b201641b24657843cae456402ebfe3272593177
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2019-09-21 12:26:49 -0500

    Add nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
-  resources :partnerships
-  resources :projects
+  resources :partnerships do
+    resources :projects
+  end
+  resources :projects do
+    resources :tasks
+  end
   resources :tasks
-  resources :contexts
+  resources :contexts do
+    resources :projects
+  end
   devise_for :users, :controllers => { omniauth_callbacks: "users/omniauth_callbacks" }
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
