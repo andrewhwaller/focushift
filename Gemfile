8499a8904368f19766e06a24dd30520804545c74
andrewhwaller <andrewhwaller@gmail.com>
2019-10-05 01:03:26 -0500

    Add routes; update gemfile

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ ruby '2.6.3'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', github: 'rails/rails'
+gem 'webpacker', '~> 4.0'
 # Use PostgreSQL as the database for Active Record
 gem 'pg'
 gem 'dotenv-rails', '~> 2.1', '>= 2.1.1'
@@ -18,7 +19,7 @@ gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # Use CoffeeScript for .coffee assets and views
-gem 'coffee-rails', '~> 4.2'
+gem 'coffee-rails', '~> 5.0'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
 gem 'turbolinks', '~> 5'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :tasks
+  resources :projects do
+    resources :tasks, shallow: true
+  end
+  resources :partnerships do
+    resources :projects, shallow: true
+  end
+  resources :contexts do
+    resources :projects, shallow: true
+  end
+  devise_for :users, :controllers => { omniauth_callbacks: "users/omniauth_callbacks" }
+  devise_scope :user do
+    get "/users/sign_out" => "devise/sessions#destroy"
+  end
+
+  root to: 'tasks#index'
+
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
