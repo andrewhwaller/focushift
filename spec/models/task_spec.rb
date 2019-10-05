dca33c6d1ea25d91dc8a9fa0ed5b35941ebb4554
andrewhwaller <andrewhwaller@gmail.com>
2019-10-05 15:14:28 -0500

    Update task_spec

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -6,6 +6,10 @@ RSpec.describe Task, type: :model do
   subject { described_class.new }
 
   it 'is valid with valid attributes' do
+    test_user = User.create!(
+      email: 'test@example.com',
+      password: 'password123')
+    subject.user_id = test_user.id
     subject.name = 'Test'
     expect(subject).to be_valid
   end
