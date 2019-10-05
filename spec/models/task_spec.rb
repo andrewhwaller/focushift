f4c540dcd9352050c972b730bbdb36088694281d
andrewhwaller <andrewhwaller@gmail.com>
2019-10-05 12:37:01 -0500

    Fix rubocop offenses

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  it "is valid with valid attributes"
-  it "is not valid without a name"
-  it "is not valid without a user"
+  it 'is valid with valid attributes'
+  it 'is not valid without a name'
+  it 'is not valid without a user'
 end
