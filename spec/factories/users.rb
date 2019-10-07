883f8343f9fbc447d373ce6c22743aa7e510af82
andrewhwaller <andrewhwaller@gmail.com>
2019-10-07 14:57:13 -0500

    Fix user factory and add sequenced emails

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,15 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
+  sequence :email do |n|
+    "test#{n}@example.com"
+  end
+
   factory :user do
     first_name { "Dale" }
     last_name { "Cooper" }
-    email { "dale@focushift.com" }
+    email
     password { "thisisatestpassword" }
+    password_confirmation { "thisisatestpassword" }
   end
 end
